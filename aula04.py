58829ffa229d7eac567efdb469e62ddeb059183d
VSJC <doouf@gmail.com>
2024-08-19 17:46:15 -0300

    Primeira aula de python no vscode

---
+++ aula04.py
@@ -0,0 +1,63 @@
+# numero1 = int(input('Informe o primeiro numero \n'))
+# numero2 = int(input('Informe o segundo numero \n'))
+
+# print(numero1 + numero2)
+# print(type(numero1))
+# print(type(numero2))
+
+
+nome = 'Jó'
+idade = 20
+altura = 1.92
+vivo = False
+
+# int()
+# float()
+# str()
+# bool()
+
+print(int(vivo))
+
+print(type(nome))
+print(type(str(idade)))
+print(type(altura))
+print(type(vivo))
+
+numero1 = int(input('Informe o primeiro numero \n'))
+numero2 = int(input('Informe o segundo numero \n'))
+
+soma = numero1 + numero2 
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+#print(soma)
+#print(subtracao)
+#print(multiplicacao)
+#print(divisao)
+
+print('A some é: ', soma )
+print('A subtracao é: '+ str(subtracao))
+print('A multiplicacao é: {} e a divisao foi {}'.format(multiplicacao, divisao) )
+
+nome = 'Jó'
+sobrenome = 'Nunes'
+
+print('O seu nome completo é '+ nome +' '+ sobrenome)
+print('O seu nome completo é', nome , sobrenome)
+print('O seu nome completo é (nome) (sobrenome) ')
+
+# Informe 10 informações de cadastro pessoal e informe um relatorio com as informações em um unico print.
+# Calcule a area de um trinagulo forncedo somente base e altura
+# Crie um algoritimo capaz de calcular o delta da formula de bascara
+
+Nome = str(input('Informe o nome \n'))
+Data de nascimento = float(input('Informe a data de nascimento \n'))
+Sexo = str(input('Informe seu sexo \n'))
+Endereço = str(input('Informe seu endereço \n'))
+Cidade = str(input('Informe sua cidade \n'))
+Estado = str(input('Informe seu estado \n'))
+Cep = float(input('Informe seu cep \n'))
+Telefone = float(input('Informe o seu telefone\n'))
+Email = str(input('Informe o email \n'))
+Nacionalidade = str(input('Informe sua nacionalidade \n'))
